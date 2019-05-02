8190e6e1704d49534648d296a2e008d2446a7725
Peter <phowland1@gmail.com>
2019-05-02 13:46:49 -0400

    Adding logging statements to server file

--- server.js
+++ server.js
@@ -27,9 +27,10 @@ io.on('connection', function (socket) {
     //first get a list of all factories
     conn.query('select * from factory', null, (error, results, fields) => {
         if (error)
-            console.log(error);
+            console.log('Database Error: ' + error);
         else {
             //organize the list into a Map<factoryId, factory>, so it is easier to add the numbers
+            console.log('Factories found: ' + results.length);
             let factories = {};
             _.each(results, (factory) => {
                 factory.numbers = [];
@@ -39,13 +40,15 @@ io.on('connection', function (socket) {
             conn.query('select * from factory_numbers',
                 (e, r, f) => {
                     if (e)
-                        console.log(e);
+                        console.log('Database Error: ' + e);
                     else {
                         //iterate through the list of numbers and assign them to the correct factory
+                        console.log('Numbers found: ' + r.length);
                         _.each(r, (num) => {
                             factories[num.factoryId].numbers.push(num.number);
                         });
                         //send the list to the new socket ONLY
+                        console.log('Factories successfully assembled');
                         socket.emit('all-factories', Object.values(factories));
                     }
                 });
@@ -54,12 +57,14 @@ io.on('connection', function (socket) {
 
     //Adds or edits a factory
     socket.on('add-factory', function (data) {
+        console.log('Adding/Editing factory');
         if (_.isNull(data.factoryId)) {
             //if the id is null, then this is a new factory so insert it
             sql = 'insert into factory (`name`, `upper`, `lower`) values(?,?,?)';
             conn.query(sql, [data.name, data.upper, data.lower], (error, result, fields) => {
                 data.factoryId = result.insertId;
                 //broadcast the new factory to all open sockets
+                console.log('Successfully added new factory: ' + data);
                 io.emit('new-factory', data);
             });
         } else {
@@ -69,9 +74,10 @@ io.on('connection', function (socket) {
                 [factory.name, factory.upper, factory.lower, factory.factoryId],
                 (error, results) => {
                     if (error) {
-                        console.log(error);
+                        console.log('Database Error: ' + error);
                     } else {
                         //broadcast the updated factory to all open sockets
+                        console.log('Successfully edited factory: ' + factory);
                         io.emit('update-factory', factory);
                     }
                 }
@@ -81,23 +87,26 @@ io.on('connection', function (socket) {
 
     //Generates N random numbers
     socket.on('generate', function (data) {
+        console.log('Generating numbers for factory: ' + data.factoryId);
         //first get the factory so we have the upper and lower limits
         conn.query('select * from factory where factoryId = ?',
             [data.factoryId],
             (error, results, fields) => {
                 if (error)
-                    console.log(error);
-                else if (results.length === 0)
-                    console.log('Error: factory not found.');
+                    console.log('Database Error: ' + error);
+                else if (_.isNull(results[0]))
+                    console.log('Database Error: factory not found: ' + data.factoryId);
                 else {
                     let factory = results[0];
+                    console.log('Found factory: ' + factory);
                     //next delete any numbers currently associated with this factory
                     conn.query('delete from factory_numbers where factoryId = ?',
                         [data.factoryId],
                         (error1, results1, fields1) => {
                             if (error1)
-                                console.log(error);
+                                console.log('Database Error: ' + error);
                             //generate the random numbers according to the upper and lower limits
+                            console.log('Deleted previous factory numbers');
                             let numbers = [];
                             factory.numbers = [];
                             for (let i = 0; i < data.count; i++) {
@@ -111,9 +120,13 @@ io.on('connection', function (socket) {
                                 (error2, results2, fields2) => {
                                     if (error2)
                                         console.log(error2);
-                                    else
-                                    //finally, broadcast the updated factory to all open sockets
+                                    else{
+                                        //finally, broadcast the updated factory to all open sockets
+                                        console.log('Successfully generated new numbers for ' + factory.name + ': ' + numbers);
                                         io.emit('update-factory', factory);
+                                        
+                                    }
+                                  
                                 });
                         });
                 }
@@ -122,20 +135,23 @@ io.on('connection', function (socket) {
 
     //Delete a factory
     socket.on('delete-factory', function (data) {
+        console.log('Deleting factory: ' + data.factoryId);
         //first delete all numbers associated with this factory (if any)
         conn.query('delete from factory_numbers where factoryId = ?',
             [data.factoryId],
             (error, results, fields) => {
                 if (error)
-                    console.log(error);
+                    console.log('Database Error: ' + error);
                 //now we can delete the factory
+                console.log('Numbers deleted for factory: ' + data.factoryId);
                 conn.query('delete from factory f where f.factoryId = ?',
                     [data.factoryId],
                     (error, result, fields) => {
                         if (error)
-                            console.log(error);
+                            console.log('Database Error: ' + error);
                         else {
                             //broadcast the deletion to all open sockets
+                            console.log('Factory successfully deleted: ' + data.factoryId);
                             io.emit('delete-factory', data.factoryId);
                         }
                     });
