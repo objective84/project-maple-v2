ad888784dd8a769d26c298df1ae79482d2fe49c5
Peter <phowland1@gmail.com>
2019-05-02 16:43:45 -0400

    Handle DB disconnects; add more validation

--- public/main/add.factory.controller.js
+++ public/main/add.factory.controller.js
@@ -14,7 +14,10 @@ projectMaple.controller('AddFactoryController', ['$scope', '$http', '$uibModalIn
                 return;
             } else
                 $scope.addEditForm.lower.$error.minMax = false;
-            if($scope.addEditForm.$invalid) return;
+            if($scope.addEditForm.$invalid) {
+                $scope.addEditForm.submitAttemped = true;
+                return;
+            }
             socket.emit("add-factory", $scope.factory);
             $uibModalInstance.close();
         }
